0f834df554ab8c273e7760abc0f63ebd7a3efd36
Joshua <Joshua@DESKTOP-RSASV0V>
2024-08-05 17:15:03 -0500

    Prueba uso de Netbeans

---
+++ src/main/java/com/mycompany/pruebagithub/PruebaGithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author Joshua
+ */
+public class PruebaGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
